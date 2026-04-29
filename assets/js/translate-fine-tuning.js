53a79a0374a70459b7b815fbf9a32bf07b3e7e9f
Tree New Bee <ijimday@gmail.com>
2026-04-29 14:11:36 +0800

    Update translate-fine-tuning.js

--- assets/js/translate-fine-tuning.js
+++ assets/js/translate-fine-tuning.js
@@ -1,5 +1,6 @@
 translate.language.setUrlParamControl('lang'); //根据URL传参控制以何种语种显示
 translate.selectLanguageTag.show = false //设置不显示语言选择的select下拉框
+translate.service.use('client.edge'); //设置使用的翻译服务
 translate.language.setLocal('chinese_simplified'); //设置本地语种为简体中文
 translate.language.translateLanguagesRange = ['chinese_simplified']; //只翻译简体中文
 translate.nomenclature.append('chinese_simplified', 'english', `
@@ -8,11 +9,12 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	小戴米铺=Jimday's Domains
 	欲买桂花同载酒，终不似，少年游。=I want to buy osmanthus to go drinking,but after all, I don’t have the traveling spirit at youth.
 	我的站点=My Web Sites
-	个=Sites
+	4个=4 Sites
 	菜=Cai
 	小趴菜=Little Pacai 
 	米市=Domains Shop
 	小戴米市=Jimday's Domains Shop
+	铺=Shop
 	网站地图=Sitemap
 `); //自定义翻译术语
 translate.ignore.text.push('中文'); //翻译时忽略指定的文字不翻译
